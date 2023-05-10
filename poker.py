f0657c3bdd998c0f2b32a1a4a749d7ead64e1eb1
noel <noelngui1@gmail.com>
2023-05-10 15:44:38 -0700

    implemented Game

--- poker.py
+++ poker.py
@@ -15,6 +15,10 @@ class Card(tpc_Card):
 
 
 class Dealer:
+    """
+    need to implement dictionary
+    """
+
     def __init__(self):
         self.hand = []
         self.suit_dict = dict()
@@ -32,6 +36,10 @@ class Dealer:
 
 
 class Player:
+    """
+    need to implement dictionary
+    """
+
     def __init__(self):
         self.hand = []
         self.suit_dict = dict()
@@ -49,26 +57,45 @@ class Player:
         self.hand.append(card)
 
 
-def take_card(text):
-    rank = text[0]
-    suit = text[1:]
-    if rank not in RANKS:
-        print("Invalid rank. Please specify something from:")
-        print(RANKS)
-        return None
-    if suit not in SUITS:
-        print("Invalid rank. Please specify something from:")
-        print(SUITS)
-        return None
-    return Card(rank, suit)
+class Game:
+    def __init__(self):
+        player = Player()
+        dealer = Dealer()
+        deck = Deck()
+        deck.shuffle()
+
+    def take_card(self, text):
+        rank = text[0]
+        suit = text[1:]
+        if rank not in RANKS:
+            print("Invalid rank. Please specify something from:")
+            print(RANKS)
+            return None
+        if suit not in SUITS:
+            print("Invalid rank. Please specify something from:")
+            print(SUITS)
+            return None
+        return Card(rank, suit)
+
+    def take_cards(self):
+        """
+        Takes in cards from input from the user and returns a list of cards
+        """
+        text = input(
+            "Give me the card you'd like to take this turn, in the form of RANKSUIT, separated by commas \n"
+        )
+        card_list_string = text.split(",")
+        print(card_list_string)
+        card_list = [self.take_card(card) for card in card_list_string]
+        print(View(card_list))
+        return card_list
+
+    def draw(self, card_list):
+        """
+        Draws a card from the deck, and
+        """
+        card = self.deck.pop()
 
 
-def take_cards():
-    text = input(
-        "Give me the card you'd like to take this turn, in the form of RANKSUIT, separated by commas \n"
-    )
-    card_list_string = text.split(",")
-    print(card_list_string)
-    card_list = [take_card(card) for card in card_list_string]
-    print(View(card_list))
-    return card_list
+game = Game()
+game.take_cards()
